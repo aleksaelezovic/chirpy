37c1d229701eb3d569260e7ad43c5e5ca587c54c
aleksaelezovic <aleksaelezovic@gmail.com>
2025-10-18 13:01:49 +0200

    add JWT creation to auth package

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,12 @@
 package auth
 
-import "github.com/alexedwards/argon2id"
+import (
+	"time"
+
+	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
 
 func HashPassword(password string) (string, error) {
 	return argon2id.CreateHash(password, argon2id.DefaultParams)
@@ -8,4 +14,28 @@ func HashPassword(password string) (string, error) {
 
 func VerifyPassword(password, hash string) (bool, error) {
 	return argon2id.ComparePasswordAndHash(password, hash)
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+	return token.SignedString([]byte(tokenSecret))
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+	id, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return uuid.Parse(id)
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,9 +1,13 @@
 package auth_test
 
 import (
+	"errors"
 	"testing"
+	"time"
 
 	"github.com/aleksaelezovic/chirpy/internal/auth"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 func TestPasswordHashing(t *testing.T) {
@@ -21,5 +25,88 @@ func TestPasswordHashing(t *testing.T) {
 	}
 	if !ok {
 		t.Errorf("Password verification failed")
+	}
+}
+
+func TestPasswordVerificationFail(t *testing.T) {
+	plainPassword := "password123"
+	hashedPassword, err := auth.HashPassword(plainPassword)
+	if err != nil {
+		t.Errorf("Error hashing password: %v", err)
+	}
+	if hashedPassword == "" {
+		t.Errorf("Hashed password is empty")
+	}
+	ok, err := auth.VerifyPassword("password1234", hashedPassword)
+	if ok {
+		t.Errorf("Expected password verification to fail")
+	}
+}
+
+func TestJWTGeneration(t *testing.T) {
+	userID := uuid.New()
+	secret := "my-super-secret-key"
+	tokenString, err := auth.MakeJWT(userID, secret, 1*time.Hour)
+	if err != nil {
+		t.Errorf("Error generating JWT token: %v", err)
+	}
+	if tokenString == "" {
+		t.Errorf("Generated token is empty")
+	}
+	tokenUserID, err := auth.ValidateJWT(tokenString, secret)
+	if err != nil {
+		t.Errorf("Error validating JWT token: %v", err)
+	}
+	if tokenUserID != userID {
+		t.Errorf("Token user ID does not match expected user ID")
+	}
+}
+
+func TestJWTExpiration(t *testing.T) {
+	userID := uuid.New()
+	secret := "my-super-secret-key"
+	tokenString, err := auth.MakeJWT(userID, secret, 1*time.Second)
+	if err != nil {
+		t.Errorf("Error generating JWT token: %v", err)
+	}
+	if tokenString == "" {
+		t.Errorf("Generated token is empty")
+	}
+	time.Sleep(2 * time.Second)
+	tokenUserID, err := auth.ValidateJWT(tokenString, secret)
+	if !errors.Is(err, jwt.ErrTokenExpired) {
+		t.Errorf("Expected token to be expired")
+	}
+	if tokenUserID != uuid.Nil {
+		t.Errorf("Token user ID should be empty")
+	}
+}
+
+func TestJWTInvalidSignature(t *testing.T) {
+	userID := uuid.New()
+	secret := "my-super-secret-key"
+	tokenString, err := auth.MakeJWT(userID, secret, 1*time.Hour)
+	if err != nil {
+		t.Errorf("Error generating JWT token: %v", err)
+	}
+	if tokenString == "" {
+		t.Errorf("Generated token is empty")
+	}
+	tokenUserID, err := auth.ValidateJWT(tokenString, "wrong-secret")
+	if !errors.Is(err, jwt.ErrSignatureInvalid) {
+		t.Errorf("Expected token to have invalid signature")
+	}
+	if tokenUserID != uuid.Nil {
+		t.Errorf("Token user ID should be empty")
+	}
+}
+
+func TestJWTInvalidToken(t *testing.T) {
+	tokenUserID, err := auth.ValidateJWT("invalid-token", "my-secret")
+	if !errors.Is(err, jwt.ErrTokenMalformed) {
+		t.Errorf("Expected token to be malformed")
+	}
+	if tokenUserID != uuid.Nil {
+		t.Errorf("Token user ID should be empty")
 	}
 }
