633cc1983e2c0401a6c54173662076008fd119ee
aleksaelezovic <aleksaelezovic@gmail.com>
2025-10-18 15:04:30 +0200

    formatting

--- sql/queries/refresh_tokens.sql
+++ sql/queries/refresh_tokens.sql
@@ -11,4 +11,6 @@ WHERE refresh_tokens.revoked_at IS NULL
 LIMIT 1;
 
 -- name: RevokeRefreshToken :exec
-UPDATE refresh_tokens SET revoked_at = NOW(), updated_at = NOW() WHERE token = $1;
+UPDATE refresh_tokens
+SET revoked_at = NOW(), updated_at = NOW()
+WHERE token = $1;
